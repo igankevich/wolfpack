bfa2457c0c53f64a1f017820613bd28c219a49dd
Ivan Gankevich <ivan@igankevich.com>
2024-10-30 08:22:12 +0100

    Write BOM WIP.

--- src/macos/bom.rs
+++ src/macos/bom.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::collections::HashSet;
 use std::collections::VecDeque;
 use std::ffi::CStr;
+use std::ffi::CString;
 use std::ffi::OsStr;
 use std::ffi::OsString;
 use std::fs::FileType;
@@ -10,10 +11,11 @@ use std::io::Read;
 use std::io::Seek;
 use std::io::SeekFrom;
 use std::io::Write;
+use std::ops::Deref;
+use std::ops::DerefMut;
 use std::os::unix::ffi::OsStrExt;
 use std::path::Path;
 use std::path::PathBuf;
-use std::str::from_utf8;
 
 use normalize_path::NormalizePath;
 use walkdir::WalkDir;
@@ -30,7 +32,42 @@ impl Bom {
             num_paths: 0,
             entries: Default::default(),
         };
-        bom_info.write(writer.by_ref())?;
+        let mut blocks = Blocks::new();
+        let mut vars = Vars::new();
+        // bom info
+        {
+            let i = blocks.write_block(writer.by_ref(), |writer| bom_info.write(writer))?;
+            vars.insert(BOM_INFO.into(), i);
+        }
+        // v index
+        {
+            let paths = Paths::null();
+            let i = blocks.write_block(writer.by_ref(), |writer| paths.write(writer))?;
+            let tree = Tree::new_v_index(i);
+            let i = blocks.write_block(writer.by_ref(), |writer| tree.write(writer))?;
+            vars.insert(V_INDEX.into(), i);
+        }
+        // hl index
+        {
+            let paths = Paths::null();
+            let i = blocks.write_block(writer.by_ref(), |writer| paths.write(writer))?;
+            let tree = Tree::null(i);
+            let i = blocks.write_block(writer.by_ref(), |writer| tree.write(writer))?;
+            vars.insert(HL_INDEX.into(), i);
+        }
+        // size 64
+        {
+            let paths = Paths::null();
+            let i = blocks.write_block(writer.by_ref(), |writer| paths.write(writer))?;
+            let tree = Tree::null(i);
+            let i = blocks.write_block(writer.by_ref(), |writer| tree.write(writer))?;
+            vars.insert(SIZE_64.into(), i);
+        }
+        // paths
+        {
+        }
+        // TODO vars
+        // TODO blocks
         // write the header
         writer.seek(SeekFrom::Start(0))?;
         let header = Header {
@@ -53,26 +90,30 @@ impl Bom {
         let index_len = header.index_len as usize;
         let vars_offset = header.vars_offset as usize;
         let vars_len = header.vars_len as usize;
-        let mut vars = read_variables(&file[vars_offset..(vars_offset + vars_len)])?;
+        let mut vars = Vars::read(&file[vars_offset..(vars_offset + vars_len)])?;
         let blocks = Blocks::read(&file[index_offset..(index_offset + index_len)])?;
         {
+            let name = BOM_INFO;
             let index = vars
-                .remove("BomInfo")
-                .ok_or_else(|| Error::other("\"BomInfo\" is missing"))?;
+                .remove(name)
+                .ok_or_else(|| Error::other(format!("{:?} is missing", name)))?;
             let bom_info = BomInfo::read(blocks.slice(index, &file)?)?;
             eprintln!("bom info {:?}", bom_info);
         }
         let mut trees = VecDeque::new();
         {
-            let name = "VIndex";
+            let name = V_INDEX;
             let index = vars
                 .remove(name)
                 .ok_or_else(|| Error::other(format!("{:?} is missing", name)))?;
             let v_index = VIndex::read(blocks.slice(index, &file)?)?;
             eprintln!("v index {:?}", v_index);
-            trees.push_back(("VIndex.index".to_string(), v_index.index));
+            let name: CString = c"VIndex.index".into();
+            trees.push_back((name, v_index.index));
         }
         let mut paths = VecDeque::new();
+        let vars = vars.vars;
+        eprintln!("vars {:?}", vars);
         for (name, index) in vars.into_iter() {
             trees.push_back((name, index));
         }
@@ -84,7 +125,7 @@ impl Bom {
                     continue;
                 }
             };
-            eprintln!("tree {:?}", tree);
+            eprintln!("tree {:?} {:?}", name.to_str(), tree);
             paths.push_back(tree.child);
         }
         // id -> data
@@ -92,11 +133,12 @@ impl Bom {
         let mut visited = HashSet::new();
         while let Some(index) = paths.pop_front() {
             if !visited.insert(index) {
-                eprintln!("loop {}", index);
+                //eprintln!("loop {}", index);
                 continue;
                 //return Err(Error::other("loop"));
             }
             let path = Paths::read(blocks.slice(index, &file)?)?;
+            //eprintln!("paths {:?}", path);
             // is_leaf == 0 means count == 1?
             for (index0, index1) in path.indices.into_iter() {
                 let child = if !path.is_leaf {
@@ -213,20 +255,61 @@ impl Header {
     }
 }
 
-fn read_variables(data: &[u8]) -> Result<HashMap<String, u32>, Error> {
-    let num_vars = u32_read(&data[0..4]) as usize;
-    let mut vars = HashMap::with_capacity(num_vars);
-    let mut data = &data[4..];
-    for _ in 0..num_vars {
-        let index = u32_read(&data[0..4]);
-        let len = data[4] as usize;
-        let name =
-            from_utf8(&data[5..(5 + len)]).map_err(|_| Error::other("invalid variable name"))?;
-        vars.insert(name.to_string(), index);
-        data = &data[(5 + len)..];
+struct Vars {
+    /// Variable name -> block index.
+    vars: HashMap<CString, u32>,
+}
+
+impl Vars {
+    fn new() -> Self {
+        Self {
+            vars: Default::default(),
+        }
     }
-    eprintln!("vars {:?}", vars);
-    Ok(vars)
+
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let num_vars = u32_read_v2(reader.by_ref())? as usize;
+        let mut vars = HashMap::with_capacity(num_vars);
+        for _ in 0..num_vars {
+            let index = u32_read_v2(reader.by_ref())?;
+            let len = u8_read(reader.by_ref())? as usize;
+            let mut name = vec![0_u8; len];
+            reader.read_exact(&mut name[..])?;
+            let name = CString::new(name).map_err(|_| Error::other("invalid variable name"))?;
+            vars.insert(name, index);
+        }
+        eprintln!("vars {:?}", vars);
+        Ok(Self { vars })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        let num_vars = self.vars.len() as u32;
+        u32_write(writer.by_ref(), num_vars)?;
+        for (name, index) in self.vars.iter() {
+            let name = name.to_bytes();
+            let len = name.len();
+            if len > u8::MAX as usize {
+                return Err(Error::other("variable name is too long"));
+            }
+            writer.write_all(&[len as u8])?;
+            writer.write_all(name)?;
+            u32_write(writer.by_ref(), *index)?;
+        }
+        Ok(())
+    }
+}
+
+impl Deref for Vars {
+    type Target = HashMap<CString, u32>;
+    fn deref(&self) -> &Self::Target {
+        &self.vars
+    }
+}
+
+impl DerefMut for Vars {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.vars
+    }
 }
 
 #[derive(Debug)]
@@ -236,6 +319,14 @@ struct Blocks {
 }
 
 impl Blocks {
+    fn new() -> Self {
+        Self {
+            // start with the null block
+            blocks: vec![Block::null()],
+            free_blocks: Default::default(),
+        }
+    }
+
     fn slice<'a>(&self, index: u32, file: &'a [u8]) -> Result<&'a [u8], Error> {
         let block = self
             .blocks
@@ -244,35 +335,116 @@ impl Blocks {
         Ok(block.slice(file))
     }
 
-    fn read(data: &[u8]) -> Result<Self, Error> {
-        let num_blocks = u32_read(&data[0..4]) as usize;
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let num_blocks = u32_read_v2(reader.by_ref())? as usize;
         let mut blocks = Vec::with_capacity(num_blocks);
-        let mut offset = 4;
         for _ in 0..num_blocks {
-            let address = u32_read(&data[offset..(offset + 4)]);
-            let len = u32_read(&data[(offset + 4)..(offset + 8)]);
-            blocks.push(Block { address, len });
-            offset += 8;
+            let block = Block::read(reader.by_ref())?;
+            blocks.push(block);
         }
-        for b in blocks.iter() {
-            eprintln!("block {:?}", b);
-        }
-        let free_blocks_bytes = &data[offset..];
-        let num_free_blocks = u32_read(&free_blocks_bytes[0..4]) as usize;
+        let num_free_blocks = u32_read_v2(reader.by_ref())? as usize;
         let mut free_blocks = Vec::with_capacity(num_free_blocks);
-        let mut offset = 4;
         for _ in 0..num_free_blocks {
-            let address = u32_read(&free_blocks_bytes[offset..(offset + 4)]);
-            let len = u32_read(&free_blocks_bytes[(offset + 4)..(offset + 8)]);
-            free_blocks.push(Block { address, len });
-            offset += 8;
-        }
-        for b in free_blocks.iter() {
-            eprintln!("free block {:?}", b);
+            let block = Block::read(reader.by_ref())?;
+            free_blocks.push(block);
         }
         Ok(Self {
             blocks,
             free_blocks,
+        })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        let num_blocks = self.blocks.len() as u32;
+        u32_write(writer.by_ref(), num_blocks + 1)?;
+        for block in self.blocks.iter() {
+            block.write(writer.by_ref())?;
+        }
+        let num_free_blocks = self.free_blocks.len() as u32;
+        // write two empty blocks at the end
+        u32_write(writer.by_ref(), num_free_blocks + 2)?;
+        for block in self.free_blocks.iter() {
+            block.write(writer.by_ref())?;
+        }
+        Block::null().write(writer.by_ref())?;
+        Block::null().write(writer.by_ref())?;
+        Ok(())
+    }
+
+    fn push(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+
+    fn write_block<W: Write + Seek, F: FnOnce(&mut W) -> Result<(), Error>>(
+        &mut self,
+        writer: W,
+        f: F,
+    ) -> Result<u32, Error> {
+        let index = self.blocks.len();
+        self.blocks.push(Block::from_write(writer, f)?);
+        Ok(index as u32)
+    }
+}
+
+#[derive(Debug)]
+struct Block {
+    // Byte offset from the start of the file.
+    offset: u32,
+    // Size in bytes.
+    len: u32,
+}
+
+impl Block {
+    fn slice<'a>(&self, file: &'a [u8]) -> &'a [u8] {
+        let i = self.offset as usize;
+        let j = i + self.len as usize;
+        &file[i..j]
+    }
+
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let offset = u32_read_v2(reader.by_ref())?;
+        let len = u32_read_v2(reader.by_ref())?;
+        Ok(Self { offset, len })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        u32_write(writer.by_ref(), self.offset)?;
+        u32_write(writer.by_ref(), self.len)?;
+        Ok(())
+    }
+
+    fn is_null(&self) -> bool {
+        self.offset == 0 && self.len == 0
+    }
+
+    fn null() -> Self {
+        Self { offset: 0, len: 0 }
+    }
+
+    fn from_write<W: Write + Seek, F: FnOnce(&mut W) -> Result<(), Error>>(
+        mut writer: W,
+        f: F,
+    ) -> Result<Self, Error> {
+        let offset = writer.stream_position()?;
+        // TODO align to block size??
+        {
+            let remainder = (offset % ALIGN as u64) as usize;
+            if remainder != 0 {
+                let n = ALIGN - remainder;
+                writer.write_all(&PADDING[..n])?;
+            }
+        }
+        f(writer.by_ref())?;
+        let len = writer.stream_position()? - offset;
+        if offset > u32::MAX as u64 {
+            return Err(Error::other("the file is too large"));
+        }
+        if len > u32::MAX as u64 {
+            return Err(Error::other("the block is too large"));
+        }
+        Ok(Self {
+            offset: offset as u32,
+            len: len as u32,
         })
     }
 }
@@ -355,23 +527,33 @@ struct Node {
 #[derive(Debug)]
 struct VIndex {
     index: u32,
-    x0: u32,
-    x1: u8,
 }
 
 impl VIndex {
-    fn read(data: &[u8]) -> Result<Self, Error> {
-        let version = u32_read(&data[0..4]);
-        if version != 1 {
+    fn new(index: u32) -> Self {
+        Self { index }
+    }
+
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let version = u32_read_v2(reader.by_ref())?;
+        if version != VERSION {
             return Err(Error::other(format!(
                 "unsupported VIndex version: {}",
                 version
             )));
         }
-        let index = u32_read(&data[4..8]);
-        let x0 = u32_read(&data[8..12]);
-        let x1 = data[12];
-        Ok(Self { index, x0, x1 })
+        let index = u32_read_v2(reader.by_ref())?;
+        let _x0 = u32_read_v2(reader.by_ref())?;
+        let _x1 = u8_read(reader.by_ref())?;
+        Ok(Self { index })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        u32_write(writer.by_ref(), VERSION)?;
+        u32_write(writer.by_ref(), self.index)?;
+        u32_write(writer.by_ref(), 0_u32)?;
+        u8_write(writer.by_ref(), 0_u8)?;
+        Ok(())
     }
 }
 
@@ -380,31 +562,57 @@ struct Tree {
     child: u32,
     block_size: u32,
     num_paths: u32,
-    x0: u8,
 }
 
 impl Tree {
-    fn read(data: &[u8]) -> Result<Self, Error> {
-        if TREE_MAGIC[..] != data[..TREE_MAGIC.len()] {
+    fn new_v_index(child: u32) -> Self {
+        Self {
+            child,
+            block_size: 128,
+            num_paths: 0,
+        }
+    }
+
+    fn null(child: u32) -> Self {
+        Self {
+            child,
+            block_size: 4096,
+            num_paths: 0,
+        }
+    }
+
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let mut magic = [0_u8; 4];
+        reader.read_exact(&mut magic[..])?;
+        if TREE_MAGIC[..] != magic[..] {
             return Err(Error::other("invalid tree magic"));
         }
-        let version = u32_read(&data[4..8]);
-        if version != 1 {
+        let version = u32_read_v2(reader.by_ref())?;
+        if version != VERSION {
             return Err(Error::other(format!(
                 "unsupported tree version: {}",
                 version
             )));
         }
-        let child = u32_read(&data[8..12]);
-        let block_size = u32_read(&data[12..16]);
-        let num_paths = u32_read(&data[16..20]);
-        let x0 = data[20];
+        let child = u32_read_v2(reader.by_ref())?;
+        let block_size = u32_read_v2(reader.by_ref())?;
+        let num_paths = u32_read_v2(reader.by_ref())?;
+        let _x = u8_read(reader.by_ref())?;
         Ok(Self {
             child,
             block_size,
             num_paths,
-            x0,
         })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        writer.write_all(&TREE_MAGIC[..])?;
+        u32_write(writer.by_ref(), VERSION)?;
+        u32_write(writer.by_ref(), self.child)?;
+        u32_write(writer.by_ref(), self.block_size)?;
+        u32_write(writer.by_ref(), self.num_paths)?;
+        u8_write(writer.by_ref(), 0_u8)?;
+        Ok(())
     }
 }
 
@@ -417,26 +625,49 @@ struct Paths {
 }
 
 impl Paths {
-    fn read(data: &[u8]) -> Result<Self, Error> {
-        let is_leaf = u16_read(&data[0..2]) != 0;
-        let count = u16_read(&data[2..4]);
-        let forward = u32_read(&data[4..8]);
-        let backward = u32_read(&data[8..12]);
-        let mut indices = Vec::new();
-        let mut data = &data[12..];
-        for _ in 0..count {
-            let index0 = u32_read(&data[0..4]);
-            let index1 = u32_read(&data[4..8]);
-            indices.push((index0, index1));
-            data = &data[8..];
+    fn null() -> Self {
+        Self {
+            forward: 0,
+            backward: 0,
+            indices: Default::default(),
+            is_leaf: true,
         }
-        eprintln!("remaining len {}", data.len());
+    }
+
+    fn read<R: Read>(mut reader: R) -> Result<Self, Error> {
+        let is_leaf = u16_read_v2(reader.by_ref())? != 0;
+        let count = u16_read_v2(reader.by_ref())?;
+        let forward = u32_read_v2(reader.by_ref())?;
+        let backward = u32_read_v2(reader.by_ref())?;
+        let mut indices = Vec::new();
+        for _ in 0..count {
+            let index0 = u32_read_v2(reader.by_ref())?;
+            let index1 = u32_read_v2(reader.by_ref())?;
+            indices.push((index0, index1));
+        }
         Ok(Self {
             forward,
             backward,
             indices,
             is_leaf,
         })
+    }
+
+    fn write<W: Write>(&self, mut writer: W) -> Result<(), Error> {
+        let is_leaf: u16 = if self.is_leaf { 1 } else { 0 };
+        u16_write(writer.by_ref(), is_leaf)?;
+        let count = self.indices.len();
+        if count > u16::MAX as usize {
+            return Err(Error::other("too many path indices"));
+        }
+        u16_write(writer.by_ref(), count as u16)?;
+        u32_write(writer.by_ref(), self.forward)?;
+        u32_write(writer.by_ref(), self.backward)?;
+        for (index0, index1) in self.indices.iter() {
+            u32_write(writer.by_ref(), *index0)?;
+            u32_write(writer.by_ref(), *index1)?;
+        }
+        Ok(())
     }
 }
 
@@ -582,26 +813,38 @@ impl TryFrom<FileType> for NodeKind {
     }
 }
 
-#[derive(Debug)]
-struct Block {
-    address: u32,
-    len: u32,
-}
-
-impl Block {
-    fn slice<'a>(&self, file: &'a [u8]) -> &'a [u8] {
-        let i = self.address as usize;
-        let j = i + self.len as usize;
-        &file[i..j]
-    }
+fn u8_read<R: Read>(mut reader: R) -> Result<u8, Error> {
+    let mut data = [0_u8; 1];
+    reader.read_exact(&mut data[..])?;
+    Ok(data[0])
 }
 
 fn u16_read(data: &[u8]) -> u16 {
     u16::from_be_bytes([data[0], data[1]])
 }
 
+fn u16_read_v2<R: Read>(mut reader: R) -> Result<u16, Error> {
+    let mut data = [0_u8; 2];
+    reader.read_exact(&mut data[..])?;
+    Ok(u16::from_be_bytes([data[0], data[1]]))
+}
+
 fn u32_read(data: &[u8]) -> u32 {
     u32::from_be_bytes([data[0], data[1], data[2], data[3]])
+}
+
+fn u32_read_v2<R: Read>(mut reader: R) -> Result<u32, Error> {
+    let mut data = [0_u8; 4];
+    reader.read_exact(&mut data[..])?;
+    Ok(u32::from_be_bytes([data[0], data[1], data[2], data[3]]))
+}
+
+fn u8_write<W: Write>(mut writer: W, value: u8) -> Result<(), Error> {
+    writer.write_all(&[value])
+}
+
+fn u16_write<W: Write>(mut writer: W, value: u16) -> Result<(), Error> {
+    writer.write_all(value.to_be_bytes().as_slice())
 }
 
 fn u32_write<W: Write>(mut writer: W, value: u32) -> Result<(), Error> {
@@ -610,9 +853,15 @@ fn u32_write<W: Write>(mut writer: W, value: u32) -> Result<(), Error> {
 
 const MAGIC: [u8; 8] = *b"BOMStore";
 const TREE_MAGIC: [u8; 4] = *b"tree";
+const V_INDEX: &CStr = c"VIndex";
+const HL_INDEX: &CStr = c"HLIndex";
+const SIZE_64: &CStr = c"Size64";
+const BOM_INFO: &CStr = c"BomInfo";
 // TODO why 512?
 const HEADER_LEN: usize = 32;
 const VERSION: u32 = 1;
+const ALIGN: usize = 4;
+const PADDING: [u8; ALIGN] = [0_u8; ALIGN];
 
 #[cfg(test)]
 mod tests {

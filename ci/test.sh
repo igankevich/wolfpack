d4d224302e94e393938f9a5d0a709e0b3cd65c4f
Ivan Gankevich <ivan@igankevich.com>
2025-02-15 11:45:58 +0100

    Workflow.

--- ci/test.sh
+++ ci/test.sh
@@ -4,9 +4,15 @@ test_integration() {
     cargo test-deb
 }
 
+test_unit() {
+    cargo test --workpace --quiet --no-run
+    cargo test --workpace --no-fail-fast -- --nocapture
+}
+
 main() {
     . ./ci/preamble.sh
-    test_integration
+    test_unit
+    #test_integration
 }
 
 main

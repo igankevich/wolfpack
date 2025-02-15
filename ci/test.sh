e3573e450149fd3ce130a26492ae93cc95f4ff9b
Ivan Gankevich <ivan@igankevich.com>
2025-02-15 13:29:22 +0100

    Lower budget.

--- ci/test.sh
+++ ci/test.sh
@@ -15,8 +15,9 @@ cargo_test_lib() {
 
 main() {
     . ./ci/preamble.sh
-    export ARBTEST_BUDGET_MS=10000
+    export ARBTEST_BUDGET_MS=2000
     DOCKER_IMAGE="ghcr.io/igankevich/wolfpack-ci-lib:latest" cargo_test_lib --nocapture
+    export ARBTEST_BUDGET_MS=10000
     DOCKER_IMAGE="ghcr.io/igankevich/wolfpack-ci-openwrt-2:latest" cargo_test_lib --nocapture --ignored opkg
     DOCKER_IMAGE="ghcr.io/igankevich/wolfpack-ci-debian:latest" cargo_test_lib --nocapture --ignored dpkg apt
     #test_integration
